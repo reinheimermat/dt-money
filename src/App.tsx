5228fa012d4e510f81ac133a65402c29f56e026e
reinheimermat <mreinheimer0@gmail.com>
2024-05-12 12:21:33 -0300

    feat: set styled-component provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Hello World</h1>
     </ThemeProvider>
   )
