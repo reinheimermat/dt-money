3de036b274084acc9962b759cfdc2bfed66aa4ac
reinheimermat <mreinheimer0@gmail.com>
2024-05-12 12:09:57 -0300

    feat: add styled global

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        :focus {
+            outline: none;
+            box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+        }
+    }
+
+`
