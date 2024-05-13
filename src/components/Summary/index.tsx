f4a7111e74d776fd93c955365fd181110555d2d5
reinheimermat <mreinheimer0@gmail.com>
2024-05-13 13:40:58 -0300

    feat: create transactions context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,13 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { SummaryCard, SummaryContainer } from './styles'
+import { useContext } from 'react'
+import { TransactionContext } from '../../contexts/TransactionContext'
 
 export function Summary() {
+  const { transactions } = useContext(TransactionContext)
+
+  console.log(transactions)
+
   return (
     <SummaryContainer>
       <SummaryCard>
